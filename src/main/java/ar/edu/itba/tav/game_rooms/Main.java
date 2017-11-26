d857419f5d1d6640a0357d0288cd5e730c78ea3c
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-11-26 03:43:03 -0300

    Set up project
    - Add pom
    - Add basic project structure
    - Add logging dependencies
    - Add compiler and assembly plugins

---
+++ src/main/java/ar/edu/itba/tav/game_rooms/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.itba.tav.game_rooms;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Entry point.
+ */
+public class Main {
+
+    /**
+     * The {@link Logger} object.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Starting application...");
+    }
+}
